03855569678687c970c6f3d2933f4b6fba51213e
aaronium112 <aaron@sphericalwave.com>
2020-01-06 12:14:23 -0400

    Initial Commit

---
+++ SQLSpeaks/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SQLSpeaks
+//
+//  Created by Aaron Anthony on 2020-01-06.
+//  Copyright © 2020 SphericalWaveSoftware. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
